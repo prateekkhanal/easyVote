acc9e6baf7ccb4e65d9cf6c0cfe6fd61205812d9
Prateek Khanal <khanalprateek101@gmail.com>
2024-02-27 07:06:07 +0545

    Add signin skeleton

---
+++ signin.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Sign in</h1>
+
+	<form action="" method="post">
+		<label for="email">Email</label><br>
+		<input type="text" name="email" id="email" placeholder="Email"><br><br>
+		<label for="password">Password</label><br>
+		<input type="password" name="password" id="password" placeholder="Password"><br><br>
+
+		<button>Signin</button>
+	</form>
+
+</body>
+</html>
