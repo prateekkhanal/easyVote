414c6be81f9efe416f96b676689765237c1991e2
Prateek Khanal <khanalprateek101@gmail.com>
2024-03-12 11:10:48 +0545

    Modify the index page with sidebars

--- index.php
+++ index.php
@@ -1,13 +1,68 @@
-<?php
-	session_start();
-if (isset($_SESSION['name'])) {
-	echo 'Hello, ' . $_SESSION['name'];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	 <meta charset="UTF-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	 <title>Three Containers Layout</title>
+	 <link rel="stylesheet" href="./css/index.css">
+	<style>
+body, html {
+    height: 100%;
+    margin: 0;
 }
-?>
+a {color: white;}
+h1 {color: white;}
 
-<h1>Hello, This is easyVote!</h1>
+.container {
+    display: flex;
+    height: 100%;
+}
 
-<h1></h1>
-<div>
-<a href="logout.php">Logout</a>
-</div>
+.left-sidebar, .right-sidebar {
+    width: 17.5%;
+    background-color: #5e6163;
+}
+
+.main-content {
+    width: 65%;
+    background-color: #3c3f42;
+}
+
+.left-sidebar, .main-content, .right-sidebar {
+    height: 100%;
+}
+</style>
+</head>
+<body>
+	 <div class="container">
+
+		   <div class="left-sidebar">
+
+				<a class="sidebar-item left-sidebar-item" href="#">Dashboard</a><br>
+				<a class="sidebar-item left-sidebar-item" href="#">Contact Us</a><br>
+				<a class="sidebar-item left-sidebar-item" href="#">About Us</a><br>
+				<a class="sidebar-item left-sidebar-item" href="#">FAQ</a><br>
+				<a class="sidebar-item left-sidebar-item" href="#">Profile</a><br>
+
+			</div>
+		  <div class="main-content">
+			<?php
+			session_start();
+			if (isset($_SESSION['name'])) {
+				echo "<h1>Hello, " . $_SESSION['name'] . "!</h1>";
+			} else {
+				echo "<h1>Hello, This is easyVote!</h1>";
+			}
+		?>
+
+
+		</div>
+			   <div class="right-sidebar">
+				<a class="sidebar-item right-sidebar-item" href="#">Create an election</a><br>
+				<a class="sidebar-item right-sidebar-item" href="#">Participated Elections</a><br>
+				<a class="sidebar-item right-sidebar-item" href="#">Switch Role</a><br>
+					<a href="logout.php" class="sidebar-item right-sidebar-item">Logout</a>
+				</div>
+		 </div>
+</body>
+</html>
