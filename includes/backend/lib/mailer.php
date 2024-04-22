9c1f12907d2983e992f9bc46e322da5dd2618a9d
Prateek Khanal <khanalprateek101@gmail.com>
2024-04-22 19:54:28 +0545

    Update the database and add the email feature

---
+++ includes/backend/lib/mailer.php
@@ -0,0 +1,48 @@
+<?php
+	 session_start();
+	//Import PHPMailer classes into the global namespace
+	//These must be at the top of your script, not inside a function
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+	require 'vendor/autoload.php';
+
+	//Create an instance; passing `true` enables exceptions
+	$mail = new PHPMailer(true);
+
+	try {
+		 //Server settings
+		 $mail->isSMTP();                                            //Send using SMTP
+		 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+		 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+		 $mail->Username   = 'easyvote101@gmail.com';                     //SMTP username
+		 $mail->Password   = 'sjtw slql rhyn pktw';                               //SMTP password
+		 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+		 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+		 //Recipients
+		 $mail->addAddress('khanalprateek101@gmail.com');               //Name is optional
+		 $mail->addAddress('easyvote101@gmail.com');               //Name is optional
+		 foreach($_FILES['documents']['name'] as $key => $value){
+		 $filename = uniqid().$_FILES['documents']['name'][$key];
+		 if (move_uploaded_file($_FILES['documents']['tmp_name'][$key], 'uploads/'.$filename)) {
+			  $mail->addAttachment('uploads/'.$filename, $_FILES['documents']['name'][$key]);
+		 }
+		
+		 }
+		
+		 //Content
+		 $mail->isHTML(true);                                  //Set email format to HTML
+		 $mail->Subject = $_POST['subject'];
+		 $mail->Body    = 'From: ' . $_POST['email-from'] . "<br><br><br>" . $_POST['message'];
+		 $mail->AltBody = $_POST['message'];
+
+		 $mail->send();
+		 $_SESSION['msg'] = 'Message has been sent';
+
+	} catch (Exception $e) {
+		  $_SESSION['msg'] = "Message could not be sent. Mailer Error: " . $mail->ErrorInfo;
+	}
+		 header("Location: ./email.php");
